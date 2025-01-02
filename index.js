31fac3d7ef8320d69aa8e070c1f7e7c4fe7b3e23
Vaishnu <vaishnu753880@gmail.com>
2025-01-02 15:19:59 +0530

    update and delete commit

--- index.js
+++ index.js
@@ -34,8 +34,9 @@ app.get('/api/products',async (req,res)=>
     {
         
         try{
-          const product = await product.find({});
-           res.send("products get successfully")
+          const productall = await product.find({});
+          res.status(200).json(productall)
+        //    res.send("products get successfully")
            }
         catch(error)
         {
@@ -43,6 +44,47 @@ app.get('/api/products',async (req,res)=>
         }
     });
     
+    app.get('/api/products/:id', async (req, res) => {
+        try {
+            const { id } = req.params; // Extract the ID from the request parameters
+            const singleproduct = await product.findById(id); // Fetch the product by ID
+            
+            if (!singleproduct) {
+                return res.status(404).json({ message: "Product not found" }); // Handle case when no product is found
+            }
+            
+            res.status(200).json(singleproduct); // Send the product as a JSON response
+        } 
+            catch(error)
+            {
+                res.status(500).json({message:error.message});
+            }
+        });
+
+
+        app.put('/api/products/:id',async (req,res)=>
+            {
+
+                try 
+                {
+                const { id } = req.params; // Extract the ID from the request parameters
+                const updateproduct = await product.findByIdAndUpdate(id,req.body); // Fetch the product by ID
+                
+                if (!updateproduct) {
+                    return res.status(404).json({ message: "Product not found" }); // Handle case when no product is found
+                }
+                const updatedproduct=await product.findById(id);
+                res.status(200).json(updatedproduct); // Send the product as a JSON response
+                
+                }
+                catch(error)
+                {
+                    res.status(500).json({message:error.message});
+                }
+       
+
+        });
+
 mongoose.connect('mongodb+srv://vaishnu753880:9566656880@node.x8h1x.mongodb.net/NODE_TEST?retryWrites=true&w=majority&appName=node')
 .then(()=> {
     console.log('xonnected to database');
